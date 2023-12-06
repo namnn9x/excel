eee303700a3f47383ce0e6a7663514c66f705ebc
namnn9x <nam664221998@gmail.com>
2023-12-06 15:08:57 -0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,45 +5,45 @@ import axios from "axios";
 import FileSaver from "file-saver";
 import { read, utils, writeFile } from 'xlsx';
 
-const ExportCSV = ({ csvData, fileName, wscols }: any) => {
-  // ******** XLSX with object key as header *************
-  // const fileType =
-  //   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-  // const fileExtension = ".xlsx";
+// const ExportCSV = ({ csvData, fileName, wscols }: any) => {
+//   // ******** XLSX with object key as header *************
+//   // const fileType =
+//   //   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+//   // const fileExtension = ".xlsx";
 
-  // const exportToCSV = (csvData, fileName) => {
-  //   const ws = XLSX.utils.json_to_sheet(csvData);
-  //   const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
-  //   const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-  //   const data = new Blob([excelBuffer], { type: fileType });
-  //   FileSaver.saveAs(data, fileName + fileExtension);
-  // };
+//   // const exportToCSV = (csvData, fileName) => {
+//   //   const ws = XLSX.utils.json_to_sheet(csvData);
+//   //   const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+//   //   const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+//   //   const data = new Blob([excelBuffer], { type: fileType });
+//   //   FileSaver.saveAs(data, fileName + fileExtension);
+//   // };
 
-  // ******** XLSX with new header *************
-  const fileType =
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-  const fileExtension = ".xlsx";
+//   // ******** XLSX with new header *************
+//   const fileType =
+//     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+//   const fileExtension = ".xlsx";
 
-  const Heading = [
-    {
-      createdAt: "Thời gian",
-      newsCategory: "Mục tin",
-      title: "Tên bài",
-      hightlight: "Điểm nổi bật",
-      content: "Nội dung"
-    }
-  ];
+//   const Heading = [
+//     {
+//       createdAt: "Thời gian",
+//       newsCategory: "Mục tin",
+//       title: "Tên bài",
+//       hightlight: "Điểm nổi bật",
+//       content: "Nội dung"
+//     }
+//   ];
 
-  const exportToCSV = (csvData: any, fileName: any, wscols: any) => {
+//   const exportToCSV = (csvData: any, fileName: any, wscols: any) => {
     
-  };
+//   };
 
-  return (
-    <button onClick={(e) => exportToCSV(csvData, fileName, wscols)}>
-      Export XLSX
-    </button>
-  );
-};
+//   return (
+//     <button onClick={(e) => exportToCSV(csvData, fileName, wscols)}>
+//       Export XLSX
+//     </button>
+//   );
+// };
 
 function formatDateTime(inputDate: any) {
   const date = new Date(inputDate);
@@ -61,13 +61,14 @@ function formatDateTime(inputDate: any) {
 
 export default function Home() {
   const [customers, setCustomers] = useState<any[]>([]);
+  const [customers2, setCustomers2] = useState<any[]>([]);
 
   const headers = [
-    { label: "Tiêu đề", key: "title" },
+    { label: "Thời gian", key: "title" },
     { label: "Chủ đề", key: "topic" },
-    { label: "Ngày đăng", key: "createdAt" },
-    { label: "Đường dẫn", key: "url" },
-    { label: "Số từ", key: "numberWord" },
+    { label: "Tiêu đề", key: "createdAt" },
+    { label: "Nổi bật", key: "url" },
+    { label: "Nội dung", key: "numberWord" },
   ];
 
   const readContent = (content: any) => {
@@ -90,6 +91,7 @@ export default function Home() {
         );
         const data = response.data;
         const test: any = [];
+        const test2: any = []
 
         for (const element of data.data) {
           if (
@@ -98,9 +100,11 @@ export default function Home() {
             element.news.length > 0
           ) {
             element.news.forEach((item: any) => {
+              const itemCreatedAt = new Date(item.created_at).getTime();
+              const startDate = new Date("2023/10/01").getTime();
+              const currentDate = new Date().getTime();
               if (
-                new Date(item.created_at).getTime() >= new Date("2023/04/01").getTime() &&
-  new Date(item.created_at).getTime() <= new Date("2023/09/30").getTime()
+                itemCreatedAt >= startDate && itemCreatedAt <= currentDate
               ) {
                 console.log(123123123123123)
                 test.push({
@@ -110,14 +114,24 @@ export default function Home() {
                   newsCategory: element.name,
                   title: item.title,
                   highlight: item.highlight,
+                  link: `https://thuanloithuongmai.vcci.com.vn/news/${item.id}/detail`,
                   content: readContent(item.content),
                 });
+
+                test2.push({
+                  createdAt: formatDateTime(
+                    new Date(item.created_at).toString()
+                  ),
+                  title: item.title,
+                  newsCategory: element.name,
+                })
               }
             });
           }
         }
         console.log(test, "---> result");
         setCustomers(test);
+        setCustomers2(test2);
         // Sửa "results" thành "result"
       } catch (error) {
         console.error("Lỗi khi gọi API: ", error);
@@ -127,11 +141,12 @@ export default function Home() {
 
   const handleExport = () => {
     const headings = [[
-        'Tiêu đề',
+        'Thời gian',
         'Chủ đề',
-        'Ngày đăng',
-        'Đường dẫn',
-        'Số từ'
+        'Tiêu đề',
+        'Nổi bật',
+        'Link',
+        'Nội dung,'
     ]];
     const wb = utils.book_new();
     const ws = utils.json_to_sheet([]);
@@ -139,6 +154,21 @@ export default function Home() {
     utils.sheet_add_json(ws, customers, { origin: 'A2', skipHeader: true });
     utils.book_append_sheet(wb, ws, 'Report');
     writeFile(wb, 'Movie Report.xlsx');
+}
+
+const handleExport2 = () => {
+  const headings = [[
+      'Thời gian',
+      'Tiêu đề',
+      'Chủ đề',
+      'Link',
+  ]];
+  const wb = utils.book_new();
+  const ws = utils.json_to_sheet([]);
+  utils.sheet_add_aoa(ws, headings);
+  utils.sheet_add_json(ws, customers2, { origin: 'A2', skipHeader: true });
+  utils.book_append_sheet(wb, ws, 'Report');
+  writeFile(wb, 'Movie Report.xlsx');
 }
 
   const wscols = [
@@ -156,10 +186,15 @@ export default function Home() {
   ];
 
   return (
-    <div className="App">
-        <div className="col-md-4 center">
-        <button onClick={handleExport} className="btn btn-primary float-right">
+    <div className="App flex">
+        <div>
+        <button onClick={handleExport} className="btn btn-primary float-right mr-10">
           Export <i className="fa fa-download"></i>
+        </button>
+        </div>
+        <div>
+        <button onClick={handleExport2} className="btn btn-primary float-right">
+          Export 2 <i className="fa fa-download"></i>
         </button>
         </div>
     </div>
